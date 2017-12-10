8df1f3b2d3c07482e25a1c3b06cdcb0ebbc3d9ef
Markus Kral <m.kral@plan-net.com>
2017-12-10 22:31:13 +0100

    merge from other branch

--- webapp2/public/javascripts/controllers/aboutcontroller.js
+++ webapp2/public/javascripts/controllers/aboutcontroller.js
@@ -2,7 +2,7 @@ var app = angular.module('DonationWebApp');
 
 function aboutController($scope) {
   // create a message to display in our view
-  $scope.message = 'This is the about Page';
+  $scope.message = 'This is the the Page, build by Markus Kral';
 };
 
 module.exports = aboutController;
--- webapp2/public/javascripts/controllers/contactcontroller.js
+++ webapp2/public/javascripts/controllers/contactcontroller.js
@@ -2,7 +2,7 @@ var app = angular.module('DonationWebApp');
 
 function contactController($scope) {
   // create a message to display in our view
-  $scope.message = 'This is the contact Page';
+  $scope.message = 'This is the contact Page of Markus Kral (markus.kral@goglemail.com)';
 };
 
 module.exports = contactController;
